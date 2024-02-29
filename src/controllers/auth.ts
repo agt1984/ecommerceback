6d811970fae7ed7cedcbc066583617bbc199a4e4
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-02-29 22:27:19 +0100

    sign up tested and working

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,23 @@
 //importa las interfaces Request y Response de Express para ser utilizadas en el código.
 import { Request, Response } from "express";
+import { prismaClient } from "../schema";
+import { hashSync } from 'bcrypt';
 
-//La función login toma dos parámetros, req y res, que representan la solicitud y la respuesta respectivamente.
-export const login = (req:Request, res:Response) => {
-    res.send('login works'); //avisa que el login funciono
+//La función sign up, para crear usuario toma dos parámetros, req y res, que representan la solicitud y la respuesta respectivamente.
+export const signup = async (req:Request, res:Response) => {
+    const {email, password, name} = req.body;
+
+    let user = await prismaClient.user.findFirst({ where: {email}})
+    if (user) {
+        throw Error('User already exists!')
+    }
+
+    user = await prismaClient.user.create({
+        data:{
+            name,
+            email,
+            password: hashSync(password, 10)
+        }
+    })
+    res.json(user)
 }
