9b3ad25affcc657d2169766113062377bf247c78
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-03-03 16:54:03 +0100

    index upgraded and working

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -3,6 +3,7 @@ import { PORT } from './secrets'
 import rootRouter from '../routes';
 import { PrismaClient } from '@prisma/client'; //configuración del cliente Prisma
 import { errorMiddleware } from '../middlewares/errors';
+import { SignUpSchema } from './users';
 
 const app:Express = express(); 
 
@@ -12,8 +13,19 @@ app.use(express.json())//se utiliza en aplicaciones Node.js que usan Express com
 app.use('/api', rootRouter);
 
 //registrar en la consola eventos relacionados con consultas específicamente
-export const prismaClient = new PrismaClient({
-    log:['query']
+//este código extiende la funcionalidad del cliente Prisma para la operación de creación de usuarios. Antes de realizar la creación, valida y 
+//formatea los datos del usuario utilizando un esquema de validación llamado SignUpSchema.
+export const prismaClient = new PrismaClient({ //instancia de cliente de prisma para hacer un crud
+    log:['query']                              //habilito las consultas de registro
+}).$extends({                                  //extiendo la funcionalidad con funciones adicionales
+    query: {                                   //extiendo las capacidades de la seccion query
+        user: {                                //extiendo las capacidades de operacion para obtener info de un usuario
+            create({args, query}) {            //sobreescribo a create
+                args.data = SignUpSchema.parse(args.data)   //aqui uso el esquema de validacion definido en 'users.ts'
+                return query(args)             //valido y devuelvo los datos
+            }
+        }
+    }
 })
 
 app.use(errorMiddleware)//se llama al middleware
